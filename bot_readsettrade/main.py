99ffe97cf3db0f84d40fcab90c69d0b6c44910e2
Sonsiri1 <sonsiriwichaiyo@gmail.com>
2026-04-02 01:20:31 +0700

    เช็คบอท + frontend ล็อคอินละไม่ไปหน้าหลัก

--- bot_readsettrade/main.py
+++ bot_readsettrade/main.py
@@ -18,30 +18,30 @@ app.include_router(analysts_router, prefix="/api", tags=["Analysts"])
 app.include_router(auth_router, prefix="/api", tags=["Auth"])
 
 
-from run_bot import run_bot
-# BOT
-def start_scheduler():
-    import schedule
-    import time
+# from run_bot import run_bot
+# # BOT
+# def start_scheduler():
+#     import schedule
+#     import time
 
-    print("บอทพร้อมทำงานแล้ว")
-    run_bot()
-    # UTC คือ 00:00 น. เวลาในไทยจะเป็น 07:00 น.
-    # ตั้งเวลา
-    schedule.every().day.at("12:00").do(run_bot) # 19:00 ตามเวลาประเทศไทย
-    schedule.every().day.at("17:30").do(run_bot) # 00:30 ตามเวลาประเทศไทย
+#     print("บอทพร้อมทำงานแล้ว")
+#     run_bot()
+#     # UTC คือ 00:00 น. เวลาในไทยจะเป็น 07:00 น.
+#     # ตั้งเวลา
+#     schedule.every().day.at("12:00").do(run_bot) # 19:00 ตามเวลาประเทศไทย
+#     schedule.every().day.at("17:30").do(run_bot) # 00:30 ตามเวลาประเทศไทย
 
-    # loop ตลอด
-    while True:
-        try:
-            schedule.run_pending()
-        except Exception as e:
-            print(f"Scheduler error: {e}")
-        time.sleep(1)
+#     # loop ตลอด
+#     while True:
+#         try:
+#             schedule.run_pending()
+#         except Exception as e:
+#             print(f"Scheduler error: {e}")
+#         time.sleep(1)
 
 
-# RUN BOT ตอน START SERVER
-@app.on_event("startup")
-def start_bot():
-    thread = threading.Thread(target=start_scheduler, daemon=True)
-    thread.start()
+# # RUN BOT ตอน START SERVER
+# @app.on_event("startup")
+# def start_bot():
+#     thread = threading.Thread(target=start_scheduler, daemon=True)
+#     thread.start()
