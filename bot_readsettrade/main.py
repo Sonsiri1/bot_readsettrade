b6b5679efa4b1bdc846f3c2fed2a070e855fdfb9
Sonsiri1 <sonsiriwichaiyo@gmail.com>
2026-04-01 23:04:17 +0700

    เปิดบอท

--- bot_readsettrade/main.py
+++ bot_readsettrade/main.py
@@ -18,29 +18,29 @@ app.include_router(analysts_router, prefix="/api", tags=["Analysts"])
 app.include_router(auth_router, prefix="/api", tags=["Auth"])
 
 
-# from run_bot import run_bot
-# # BOT
-# def start_scheduler():
-#     import schedule
-#     import time
+from run_bot import run_bot
+# BOT
+def start_scheduler():
+    import schedule
+    import time
 
-#     print("บอทพร้อมทำงานแล้ว")
+    print("บอทพร้อมทำงานแล้ว")
 
-#     # ตั้งเวลา
-#     schedule.every().day.at("19:00").do(run_bot)
-#     schedule.every().day.at("23:00").do(run_bot)
+    # ตั้งเวลา
+    schedule.every().day.at("19:00").do(run_bot)
+    schedule.every().day.at("23:00").do(run_bot)
 
-#     # loop ตลอด
-#     while True:
-#         try:
-#             schedule.run_pending()
-#         except Exception as e:
-#             print(f"Scheduler error: {e}")
-#         time.sleep(1)
+    # loop ตลอด
+    while True:
+        try:
+            schedule.run_pending()
+        except Exception as e:
+            print(f"Scheduler error: {e}")
+        time.sleep(1)
 
 
-# # RUN BOT ตอน START SERVER
-# @app.on_event("startup")
-# def start_bot():
-#     thread = threading.Thread(target=start_scheduler, daemon=True)
-#     thread.start()
+# RUN BOT ตอน START SERVER
+@app.on_event("startup")
+def start_bot():
+    thread = threading.Thread(target=start_scheduler, daemon=True)
+    thread.start()
--- bot_readsettrade/run_bot.py
+++ bot_readsettrade/run_bot.py
@@ -1,51 +1,7 @@
 # import os
-from arrow import now
-import schedule
-import time
-from datetime import datetime
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from jobs.bot import start_bot
-
-def run_bot():
-    now = datetime.now()
-    
-    # ข้ามวันหยุดสุดสัปดาห์
-    if now.weekday() >= 5:
-        print("วันหยุด ไม่รัน")
-        return
-    
-    print(f"\nเริ่มรันบอท: {now.strftime('%H:%M:%S')}")
-    
-    start = datetime.now()
-
-    try:
-        start_bot()
-    except Exception as e:
-        print(f"บอทพัง: {e}")
-
-    # จบการจับเวลา
-    duration = datetime.now() - start
-    print(f"ใช้เวลาไป: {duration}")
-
-
-if __name__ == "__main__":
-    print("โหลด .env สำเร็จ")
-    
-    # ลองว่าได้ไหม
-    # print(f"APP_ID ที่อ่านได้คือ: {os.getenv('STT-OPENAPI-AUTH-BROKER-ID')}") 
-    # start_bot()
-    # รันหลัง 1 ทุ่ม
-    schedule.every().day.at("19:00").do(run_bot)
-    schedule.every().day.at("23:00").do(run_bot)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
 # from arrow import now
+# import schedule
+# import time
 # from datetime import datetime
 # from dotenv import load_dotenv
 
@@ -53,17 +9,16 @@ if __name__ == "__main__":
 
 # from jobs.bot import start_bot
 
-
 # def run_bot():
-#     now_time = datetime.now()
-
-#     # ข้ามวันหยุด
-#     if now_time.weekday() >= 5:
+#     now = datetime.now()
+    
+#     # ข้ามวันหยุดสุดสัปดาห์
+#     if now.weekday() >= 5:
 #         print("วันหยุด ไม่รัน")
 #         return
-
-#     print(f"\nเริ่มรันบอท: {now_time.strftime('%H:%M:%S')}")
-
+    
+#     print(f"\nเริ่มรันบอท: {now.strftime('%H:%M:%S')}")
+    
 #     start = datetime.now()
 
 #     try:
@@ -71,5 +26,50 @@ if __name__ == "__main__":
 #     except Exception as e:
 #         print(f"บอทพัง: {e}")
 
+#     # จบการจับเวลา
 #     duration = datetime.now() - start
 #     print(f"ใช้เวลาไป: {duration}")
+
+
+# if __name__ == "__main__":
+#     print("โหลด .env สำเร็จ")
+    
+#     # ลองว่าได้ไหม
+#     # print(f"APP_ID ที่อ่านได้คือ: {os.getenv('STT-OPENAPI-AUTH-BROKER-ID')}") 
+#     # start_bot()
+#     # รันหลัง 1 ทุ่ม
+#     schedule.every().day.at("19:00").do(run_bot)
+#     schedule.every().day.at("23:20").do(run_bot)
+
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
+
+from arrow import now
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from jobs.bot import start_bot
+
+
+def run_bot():
+    now_time = datetime.now()
+
+    # ข้ามวันหยุด
+    if now_time.weekday() >= 5:
+        print("วันหยุด ไม่รัน")
+        return
+
+    print(f"\nเริ่มรันบอท: {now_time.strftime('%H:%M:%S')}")
+
+    start = datetime.now()
+
+    try:
+        start_bot()
+    except Exception as e:
+        print(f"บอทพัง: {e}")
+
+    duration = datetime.now() - start
+    print(f"ใช้เวลาไป: {duration}")
