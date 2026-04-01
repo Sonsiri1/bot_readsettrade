81b316280151cbf15b9efa0607cf8c5c3cb77e0f
Sonsiri1 <sonsiriwichaiyo@gmail.com>
2026-04-01 21:01:08 +0700

    แก้ database ใช้บน server

--- bot_readsettrade/core/database.py
+++ bot_readsettrade/core/database.py
@@ -6,15 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-host=os.getenv('DB_HOST')
-database=os.getenv('DB_NAME')
-user=os.getenv('DB_USER')
-password=os.getenv('DB_PASSWORD')
-port=os.getenv('DB_PORT')
+# host=os.getenv('DB_HOST')
+# database=os.getenv('DB_NAME')
+# user=os.getenv('DB_USER')
+# password=os.getenv('DB_PASSWORD')
+# port=os.getenv('DB_PORT')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database}"
 
-engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
+# engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
