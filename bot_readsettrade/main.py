0aa57d7a86dcb7221c74ffef22c46aad4d094823
Sonsiri1 <sonsiriwichaiyo@gmail.com>
2026-04-01 22:36:21 +0700

    เพิ่ม frontend server

--- bot_readsettrade/main.py
+++ bot_readsettrade/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://bot-readsettrade-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
